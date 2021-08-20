35a7c30d014eff12dafb75f0601150b009d15753
Bruce Ediger <bediger8@gmail.com>
2021-08-20 16:03:13 -0600

    read stdin a byte at a time

--- r1.go
+++ r1.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+/*
+Bytes   |           Byte format
+-----------------------------------------------
+   1     | 0xxxxxxx
+   2     | 110xxxxx 10xxxxxx
+   3     | 1110xxxx 10xxxxxx 10xxxxxx
+   4     | 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+*/
 
 func main() {
-	fmt.Println("vim-go")
+	var buf [1]byte
+
+	byteCount := 0
+
+	for {
+		n, err := os.Stdin.Read(buf[:])
+		if n == 0 {
+			// end-of-file
+			break
+		}
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+			break
+		}
+		byteCount++
+	}
+	fmt.Printf("Read %d bytes\n", byteCount)
 }
